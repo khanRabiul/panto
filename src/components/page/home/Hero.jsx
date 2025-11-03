e3645d278d0d4672e5988e207f4de240d076db39
KhanRabiul <krabiulislam2@gamil.com>
2025-11-03 23:31:37 +0600

    [Hero] static tooltip

--- src/components/page/home/Hero.jsx
+++ src/components/page/home/Hero.jsx
@@ -25,6 +25,17 @@ const Hero = () => {
 
       {/* bottom blur */}
       <div className="absolute inset-x-0 bottom-0 h-3/4 bg-gradient-to-t from-white via-transparent to-transparent blur-sm -mb-2"></div>
+
+      {/* hover tooltip */}
+      <div className="absolute bottom-45 left-24 group">
+        <button className="relative  p-4 rounded-full bg-white/35 text-xl border"></button>
+        <div className="absolute hidden bottom-full left-1/2 transform -translate-x-1/2 mb-3 p-2 space-x-1 rounded-lg bg-white/25 shadow-md group-hover:flex">
+        <div className="size-6 bg-red-700 border border-white rounded-full"></div>
+        <div className="size-6 bg-green-700 border border-white rounded-full"></div>
+        <div className="size-6 bg-blue-700 border border-white rounded-full"></div>
+        <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-white/35"></div>
+        </div>
+      </div>
     </section>
   );
 };
