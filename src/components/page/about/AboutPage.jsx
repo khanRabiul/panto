726affca5c574def92a187eb01f7bf00abfec455
khanRabiul <krabiulislam2@gmail.com>
2025-11-17 15:06:25 +0600

    [About] about page and section added

--- src/components/page/about/AboutPage.jsx
+++ src/components/page/about/AboutPage.jsx
@@ -1,6 +1,16 @@
+import bgImage from '../../../assets/about-background.jpg'
+import Exprience from '../home/Experience';
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <section>
+      <div className='w-full h-[400px] bg-no-repeat bg-cover flex items-center justify-center text-white' style={{backgroundImage: `url(${bgImage})`}}>
+        <div>
+          <h1 className='text-5xl font-bold underline underline-offset-4'>About Us</h1>
+        </div>
+      </div>
+        <Exprience />
+    </section>
   );
 };
 
