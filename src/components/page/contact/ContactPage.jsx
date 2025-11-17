a013aba615f9538920173b815a510b279f2d8b9f
khanRabiul <krabiulislam2@gmail.com>
2025-11-17 15:11:24 +0600

    [ContactPage] contact us page or section added

--- src/components/page/contact/ContactPage.jsx
+++ src/components/page/contact/ContactPage.jsx
@@ -1,6 +1,21 @@
+import contactBgImage from '../../../assets/contact-background.jpg'
+import Materials from '../home/Materails';
+import Testimonials from '../home/Testimonials';
+
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <section>
+      <div
+        className="w-full h-[400px] bg-no-repeat bg-cover flex items-center justify-center text-white"
+        style={{ backgroundImage: `url(${contactBgImage})` }}
+      >
+        <div>
+          <h1 className="text-5xl font-bold underline underline-offset-4">Contact Us</h1>
+        </div>
+      </div>
+      <Materials />
+      <Testimonials/>
+    </section>
   );
 };
 
